7bdd0e4015871211e4dea2a3da74879f7136dbdf
khv1one <a.khvostovets@gmail.com>
2020-04-27 18:55:41 +0300

    Init login service

---
+++ services/login_service/src/main/kotlin/org/venexer/login_service/LoginServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.venexer.login_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoginServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoginServiceApplication>(*args)
+}
---
+++ services/login_service/src/test/kotlin/org/venexer/login_service/LoginServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.venexer.login_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
